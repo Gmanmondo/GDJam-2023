0885594190a693d602a18963a719f61288fe65ec
Gmanmondo <gabriel.mondelli@ontariotechu.net>
2023-10-21 21:45:47 -0400

    Got the ambient sound working

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+
+public class AudioManager : MonoBehaviour
+{
+    private EventInstance ambienceEventInstance;
+
+    public static AudioManager instance {get; private set;}
+
+    public void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("Found more than one Audio Manager in the scene.");
+        }
+        instance = this;
+    }
+
+    private void Start()
+    {
+        InitializeAmbience(FMODEvents.instance.ambience);
+    }
+
+    private void InitializeAmbience(EventReference ambienceEventReference)
+    {
+        ambienceEventInstance = CreateInstance(ambienceEventReference);
+        ambienceEventInstance.start();
+        Debug.Log("hello");
+    }
+
+    public void PlayOneShot(EventReference sound, Vector3 worldPos)
+    {
+        RuntimeManager.PlayOneShot(sound, worldPos);
+    }
+
+    public EventInstance CreateInstance(EventReference eventReference)
+    {
+        EventInstance eventInstance = RuntimeManager.CreateInstance(eventReference);
+        return eventInstance;
+    }
+}
---
+++ Assets/Scripts/Audio/FMODEvents.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using FMODUnity;
+using UnityEngine;
+
+public class FMODEvents : MonoBehaviour
+{
+    [field: Header("Ambience")]
+    [field: SerializeField] public EventReference ambience {get; private set;}
+
+    public static FMODEvents instance {get; private set;}
+
+    public void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("Found more than one Audio Manager in the scene.");
+        }
+        instance = this;
+    }
+}
