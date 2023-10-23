da51bce0c7849328c1b6f7aa5e0adf069dc90c8a
Alexander Waiganjo <alexander.waiganjo@ontariotechu.net>
2023-10-23 19:29:24 -0400

    Dialogue
    Lot of dialogue fixes

--- Assets/Scripts/UI Elements/UIManager.cs
+++ Assets/Scripts/UI Elements/UIManager.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public bool speaking = true;
+    public TextMeshProUGUI [] text;
     void Start()
     {
-        
+        InvokeRepeating("Dialogue",1f,3f);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Dialogue()
+    {
+        float rnd;
+        rnd = Random.Range(0, 3);
+
+        switch (rnd)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+        }
     }
 }
--- Assets/Scripts/UI Elements/WardenDialogueBox.cs
+++ Assets/Scripts/UI Elements/WardenDialogueBox.cs
@@ -1,24 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WardenDialogueBox : MonoBehaviour
 {
     public GameObject camera;
     public float distance = 5f;
+    public GameObject inGameText;
+    public Image UITextbox;
+    public TextMeshProUGUI UIText;
+    public GameObject wardenBox;
+    private UIManager manager;
 
     void Start()
     {
-        
+        manager = GameObject.Find("UIManager").GetComponent<UIManager>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         RaycastHit seeDialogue;
-        if (Physics.Raycast(transform.position, transform.forward, out seeDialogue, Mathf.Infinity))
+        //if (Physics.Raycast(camera.transform.position, camera.transform.forward, out seeDialogue, 10f))
+        if(manager.speaking | !inGameText.GetComponent<Renderer>().isVisible)
         {
-            
+            ShowUI();
         }
+        else
+        {
+            HideUI();
+        }
+        FollowPlayer();
+        //Debug.Log(seeDialogue.transform.gameObject.name);
+        Debug.Log(UITextbox.color.a);
+    }
+
+    void ShowUI()
+    {
+        var tempColor = UITextbox.color;
+        var tempColor1 = UIText.color;
+        tempColor.a = 1f;
+        tempColor1.a = 1f;
+        UITextbox.color = tempColor;
+        UIText.color = tempColor1;
+    }
+
+    void HideUI()
+    {
+        var tempColor = UITextbox.color;
+        var tempColor1 = UIText.color;
+        tempColor.a = 0f;
+        tempColor1.a = 0f;
+        UITextbox.color = tempColor;
+        UIText.color = tempColor1;
+    }
+
+    void FollowPlayer()
+    {
+        wardenBox.transform.LookAt(PlayerSingleton._pRef.pTrans); 
     }
 }
